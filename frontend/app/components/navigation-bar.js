9a92b2da021403a5eede6ff794fccd0262ac45be
Marilu <marilu.pebes@10pines.com>
2018-08-16 12:42:59 -0300

    Mejora visualmente el header, ubicando los titulos, y el background

--- frontend/app/components/navigation-bar.js
+++ frontend/app/components/navigation-bar.js
@@ -4,7 +4,7 @@ import Application from "../concepts/application";
 
 export default Ember.Component.extend(AuthenticatorInjected, {
   tagName: 'nav',
-  classNames: ['indigo'],
+  classNames: [],
   application: Application.create(),
   actions: {
     logout: function () {
