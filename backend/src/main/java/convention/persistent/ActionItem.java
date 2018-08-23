dff01856e3f4d892677212263747950a9351de06
erikgervas <bernaderi@hotmail.com>
2018-08-23 11:36:13 -0300

    Algo que faltaba para que funcione

--- backend/src/main/java/convention/persistent/ActionItem.java
+++ backend/src/main/java/convention/persistent/ActionItem.java
@@ -1,5 +1,6 @@
 package convention.persistent;
 
+import ar.com.kfgodel.temas.notifications.ActionItemMailSender;
 import ar.com.kfgodel.temas.notifications.ActionItemObserver;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
@@ -27,8 +28,12 @@ public class ActionItem  extends PersistableSupport {
     private String descripcion;
     public static final String descripcion_FIELD = "descripcion";
 
-    @OneToMany
+    @Transient
     private List<ActionItemObserver> observers = new ArrayList<>();
+
+    public ActionItem() {
+        this.addObserver(new ActionItemMailSender());
+    }
 
     public List<Usuario> getResponsables() {
         return responsables;
