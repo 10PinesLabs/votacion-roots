81d040df5790e9cbd5759ea319953ebabc40721d
ailen munoz <ailenmunozz@gmail.com>
2018-08-27 12:19:44 -0300

    log en onSetResponsables del stubMailSender

--- backend/src/main/java/ar/com/kfgodel/temas/notifications/ActionItemStubMailSender.java
+++ backend/src/main/java/ar/com/kfgodel/temas/notifications/ActionItemStubMailSender.java
@@ -1,11 +1,16 @@
 package ar.com.kfgodel.temas.notifications;
 
+import ar.com.kfgodel.temas.config.HerokuPriorityConfigSelector;
 import convention.persistent.ActionItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ActionItemStubMailSender extends MailerObserver {
 
+    public static Logger LOG = LoggerFactory.getLogger(HerokuPriorityConfigSelector.class);
+
     @Override
     public void onSetResponsables(ActionItem actionItem) {
-
+        LOG.info("Se ejecuta StubMailSender. No envía mails");
     }
 }
