30e50fc61e57abdc1c90684c8cec40d92aed7eb2
marilu <malupeb@hotmail.com.ar>
2018-08-31 16:31:39 -0300

    Sigo analizando como solucionar a esto

--- frontend/app/components/vot-star.js
+++ frontend/app/components/vot-star.js
@@ -2,15 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  primerVoto: Ember.computed('reunion','temasVotados', function() {
+  primerVoto: Ember.computed('updatingVotos','temasVotados', function() {
     return this.temasVotados[0];
   }),
 
-  segundoVoto: Ember.computed('reunion','temasVotados', function() {
+  segundoVoto: Ember.computed('updatingVotos','temasVotados', function() {
     return this.temasVotados[1];
   }),
 
-  tercerVoto: Ember.computed('reunion.tema','temasVotados', function() {
+  tercerVoto: Ember.computed('updatingVotos','temasVotados', function() {
     return this.temasVotados[2];
   }),
 
