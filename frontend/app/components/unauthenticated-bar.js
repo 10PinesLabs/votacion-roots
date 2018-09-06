018c3d287346554609e6cdf399f8a5be76c6c75c
ailen munoz <ailenmunozz@gmail.com>
2018-09-06 12:30:41 -0300

    elimino componente de unauthenticated-bar que no se usa mas

--- frontend/app/components/unauthenticated-bar.js
+++
@@ -1,8 +0,0 @@
-import Ember from "ember";
-import Application from "../concepts/application";
-
-export default Ember.Component.extend({
-  tagName: 'nav',
-  classNames: ['indigo'],
-  application: Application.create(),
-});
