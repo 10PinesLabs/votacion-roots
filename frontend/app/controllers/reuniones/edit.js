aa560f7c43ea356d7be0b6ebd7d9610be35febea
erikgervas <bernaderi@hotmail.com>
2018-09-10 14:38:14 -0300

    ; que faltaban

--- frontend/app/controllers/reuniones/edit.js
+++ frontend/app/controllers/reuniones/edit.js
@@ -354,7 +354,7 @@ export default Ember.Controller.extend(ReunionServiceInjected, TemaServiceInject
   _recargarTemas() {
     this._recargarReunion().then(() =>
       this.set('updatingVotos', false)
-    )
+    );
   },
 
   _usarInstanciasDeTemas(reunion, usuarioActual) {
@@ -365,8 +365,7 @@ export default Ember.Controller.extend(ReunionServiceInjected, TemaServiceInject
       var tema = Tema.create(objetoEmber);
       temasPropuestos[i] = tema;
     }
-  }
-  ,
+  },
 
   _filtrarTemasGeneradosPorTemasGenerales(reunion) {
     var temasFiltrados = reunion.get('temasPropuestos').filter(function (tema) {
