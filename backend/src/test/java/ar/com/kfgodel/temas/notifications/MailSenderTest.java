b6bc51146308e604d7005fdfe56c630e1f17c0d4
ailen munoz <ailenmunozz@gmail.com>
2018-09-11 11:42:32 -0300

    separo responsabilidades en los tests

--- backend/src/test/java/ar/com/kfgodel/temas/notifications/MailSenderTest.java
+++ backend/src/test/java/ar/com/kfgodel/temas/notifications/MailSenderTest.java
@@ -20,9 +20,6 @@ import static org.mockito.Mockito.times;
 public class MailSenderTest {
 
     RealMailSender mockMailSender;
-    ActionItemMailSender realActionItemMailSender;
-    ActionItemMailSender stubActionItemMailSender;
-    ActionItem actionItem;
     String port = "SMTP_PORT";
     String host = "SMTP_HOST";
 
@@ -32,79 +29,11 @@ public class MailSenderTest {
         PowerMockito.when(System.getenv(port)).thenReturn("587");
         PowerMockito.when(System.getenv(host)).thenReturn("smtp.gmail.com");
         mockMailSender = Mockito.mock(RealMailSender.class);
-        realActionItemMailSender = new ActionItemMailSender(new RealMailSender());
-        stubActionItemMailSender = new ActionItemMailSender(mockMailSender);
-        actionItem = new ActionItem();
     }
 
     @Test
-    public void elActionItemMailSenderNoEnviaElMailSiElActionItemNoTieneDescripcion(){
-        actionItem.addObserver(realActionItemMailSender);
-        try{
-            realActionItemMailSender.onSetResponsables(actionItem);
-            fail();
-        }catch(Exception emptyActionException){
-            assertThat(emptyActionException.getMessage()).isEqualTo(realActionItemMailSender.EMPTY_ITEM_ACTION_EXCEPTION);
-        }
-    }
-
-    @Test
-    public void elActionItemMailSenderNoEnviaMailSiElActionItemNoTieneResponsables(){
-        actionItem.addObserver(realActionItemMailSender);
-        actionItem.setDescripcion("Una descripción");
-        try {
-            realActionItemMailSender.onSetResponsables(actionItem);
-            fail();
-        }catch(Exception emptyActionException) {
-            assertThat(emptyActionException.getMessage()).isEqualTo(realActionItemMailSender.EMPTY_ITEM_ACTION_EXCEPTION);
-        }
-    }
-
-    @Test
-    public void unUsuarioConMailNuloOVacioEsResponsableDeUnActionItemPeroNoSeLeEnviaMail(){
-        Usuario usuarioSinMail = Usuario.create("pepe", "pepe","pepe", "1000", null);
-        Usuario otroUsuarioSinMail = Usuario.create("juan", "juan","juan", "3000", "");
-        TemaDeMinuta temaDeMinuta = new TemaDeMinuta();
-        TemaDeReunion tema = new TemaDeReunion();
-        tema.setTitulo("titulo");
-        temaDeMinuta.setTema(tema);
-        Reunion reunion = new Reunion();
-        reunion.setId(1L);
-        tema.setReunion(reunion);
-        Minuta minuta = new Minuta();
-        minuta.setReunion(reunion);
-        temaDeMinuta.setMinuta(minuta);
-        actionItem.setDescripcion("Una descripcion");
-        actionItem.addObserver(stubActionItemMailSender);
-        actionItem.setTema(temaDeMinuta);
-
-        actionItem.setResponsables(Arrays.asList(usuarioSinMail, otroUsuarioSinMail));
-
-        Mockito.verify(mockMailSender,times(0)).sendMail(usuarioSinMail.getMail(),stubActionItemMailSender.getAsunto(actionItem), stubActionItemMailSender.getDescripcion(actionItem));
-        Mockito.verify(mockMailSender,times(0)).sendMail(otroUsuarioSinMail.getMail(), stubActionItemMailSender.getAsunto(actionItem), stubActionItemMailSender.getDescripcion(actionItem));
-
-    }
-
-    @Test
-    public void elActionItemMailSenderEnviaMailATodosLosResponsables(){
-        actionItem.removeAllObservers();
-        actionItem.addObserver(stubActionItemMailSender);
-        actionItem.setDescripcion("Una descripcion");
-        TemaDeMinuta temaDeMinuta = new TemaDeMinuta();
-        TemaDeReunion tema = new TemaDeReunion();
-        tema.setTitulo("titulo");
-        Reunion reunion = new Reunion();
-        reunion.setId(1L);
-        tema.setReunion(reunion);
-        temaDeMinuta.setTema(tema);
-        Minuta minuta = new Minuta();
-        minuta.setReunion(reunion);
-        temaDeMinuta.setMinuta(minuta);
-        Usuario unUsuario = Usuario.create("pedro", "pedro","pedro","9000","pedro@10pines.com");
-        actionItem.setTema(temaDeMinuta);
-        actionItem.setResponsables(Arrays.asList(unUsuario));
-
-        Mockito.verify(mockMailSender,atLeastOnce())
-                .sendMail(unUsuario.getMail(), stubActionItemMailSender.getAsunto(actionItem), stubActionItemMailSender.getDescripcion(actionItem));
+    public void elActionItemMailSenderEnviaMail(){
+        mockMailSender.sendMail("unMail", "unAsunto", "unaDescripcion");
+        Mockito.verify(mockMailSender,atLeastOnce()).sendMail("unMail", "unAsunto", "unaDescripcion");
     }
 }
