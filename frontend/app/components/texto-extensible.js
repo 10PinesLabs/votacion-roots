3f90342468a2987658049994008b4d3765717bfa
Damian Pereira <damian.pereira@10pines.com>
2019-08-01 12:13:42 -0300

    Cambio de animar con JS a animar solo con CSS

--- frontend/app/components/texto-extensible.js
+++ frontend/app/components/texto-extensible.js
@@ -1,15 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  didInsertElement() {
-    Ember.run.schedule('afterRender', this, this.measureDescriptionAndSet);
-  },
-  measureDescriptionAndSet(){
-    this.set(
-      "descriptionMaxHeight",
-      Ember.String.htmlSafe("max-height: " +
-        this.get("element").getElementsByClassName("descripcion-minuta")[0].scrollHeight +
-        "px")
-    );
-  }
 });
