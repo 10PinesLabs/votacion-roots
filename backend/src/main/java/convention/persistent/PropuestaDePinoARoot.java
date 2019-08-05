c39b338a9750d9d0be5dff163590c4aaf8c0ab38
Angel Yan <angelyan96@gmail.com>
2019-08-05 18:52:32 -0300

    Crea TO para PropuestaDePinoARoot

--- backend/src/main/java/convention/persistent/PropuestaDePinoARoot.java
+++ backend/src/main/java/convention/persistent/PropuestaDePinoARoot.java
@@ -6,10 +6,15 @@ import javax.persistence.ManyToOne;
 @Entity
 public class PropuestaDePinoARoot extends PersistableSupport {
 
+    public static final String pino_FIELD = "pino";
+    public static final String sponsor_FIELD = "sponsor";
     @ManyToOne
     private Usuario sponsor;
 
     private String pino;
+
+    public PropuestaDePinoARoot() {
+    }
 
     public PropuestaDePinoARoot(String unPinoAProponer, Usuario unSponsor) {
         pino = unPinoAProponer;
