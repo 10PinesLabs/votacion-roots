d273bd68652a9f70b3e05453662b1f23e5421dcb
Angel Yan <angelyan96@gmail.com>
2019-08-05 14:42:08 -0300

    Subclasifica TemaDeReunion en TemaDeReunionConDescripción
    Posteriormente se refactorizará la jerarquía para que TemaDeReunion sea una clase abstracta con las subclases concretas TemaDeReunionConDescripcion y TemaParaProponerPinosComoRoot

---
+++ backend/src/main/java/convention/persistent/TemaDeReunionConDescripcion.java
@@ -0,0 +1,20 @@
+package convention.persistent;
+
+public class TemaDeReunionConDescripcion extends TemaDeReunion {
+    public static TemaDeReunionConDescripcion create(
+            Usuario unAutor,
+            DuracionDeTema unaDuracion,
+            ObligatoriedadDeTema unaObligatoriedad,
+            String unTitulo,
+            String unaDescripcion) {
+
+
+        TemaDeReunionConDescripcion tema = new TemaDeReunionConDescripcion();
+        tema.setAutor(unAutor);
+        tema.setDuracion(unaDuracion);
+        tema.setObligatoriedad(unaObligatoriedad);
+        tema.setTitulo(unTitulo);
+        tema.setDescripcion(unaDescripcion);
+        return tema;
+    }
+}
