bd8c4805b5b76312abaa8730b6ed4460234bbe41
Damian Pereira <damian.pereira@10pines.com>
2019-08-30 11:30:23 -0300

    Fix antiguedad de tema relativa

--- frontend/app/components/header-tema.js
+++ frontend/app/components/header-tema.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   antiguedad: Ember.computed('tema', 'antiguedadRelativa', function(){
     if (this.get('antiguedadRelativa')){
-      return "hace" + this.tema.antiguedadDePropuesta;
+      return this.tema.get('antiguedadDePropuesta');
     } else {
       return "el " + this.tema.get('fechaDePrimeraPropuesta');
     }
