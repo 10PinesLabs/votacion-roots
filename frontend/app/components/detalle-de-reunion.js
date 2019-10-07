ce9b5a57cde03f773b6d80d7c7f270541fd1c4e7
ailen munoz <ailenmunozz@gmail.com>
2019-10-07 14:50:55 -0300

    Obtiene el titulo de las tablas dependiendo si la reunion esta cerrada o abierta

--- frontend/app/components/detalle-de-reunion.js
+++ frontend/app/components/detalle-de-reunion.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  tableTitles: Ember.computed('reunionCerrada', function(){
+    const titulosDefault = '#, Título, Autor, Duración, Votos';
+    const titulosConTemasTratados = titulosDefault + ', Tratado';
+    return this.get('reunionCerrada') ? titulosConTemasTratados : titulosDefault;
+  })
+
 });
