af1594b395a9077386e6f0c449d45c0bd8074adc
ailen munoz <ailenmunozz@gmail.com>
2019-10-09 09:43:33 -0300

    Agrega los botones secundarios del detalle de la reunion junto con sus acciones.

--- frontend/app/components/detalle-de-reunion.js
+++ frontend/app/components/detalle-de-reunion.js
@@ -1,11 +1,39 @@
 import Ember from 'ember';
+import NavigatorInjected from "../mixins/navigator-injected";
+import ReunionServiceInjected from "../mixins/reunion-service-injected";
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(NavigatorInjected, ReunionServiceInjected, {
 
-  tableTitles: Ember.computed('reunionCerrada', function(){
+  tableTitles: Ember.computed('reunionCerrada', function () {
     const titulosDefault = '#, Título, Autor, Duración, Votos';
     const titulosConTemasTratados = titulosDefault + ', Tratado';
     return this.get('reunionCerrada') ? titulosConTemasTratados : titulosDefault;
-  })
+  }),
 
+  actions: {
+    masOpciones() {
+      const mostrarOpciones = this.get('showOptions');
+      this.set('showOptions', !mostrarOpciones);
+    },
+
+    eliminarReunion(reunion) {
+      this.reunionService().removeReunion(reunion)
+        .then(() => this.recargarLista());
+    },
+
+    compartirReunion(reunion) {
+      const baseUrl = window.location.host;
+      const linkToShare = baseUrl + "/minuta/" + reunion.id + "/ver";
+      navigator.clipboard.writeText(linkToShare)
+        .then(() => {
+          this.set('showCopyToClipboardMessage', true);
+          setTimeout(() => this.set('showCopyToClipboardMessage', false), 1000);
+        }).catch(() => console.error("No se pudo copiar"));
+    },
+
+    editarReunion(reunion) {
+      this.navigator().navigateToReunionesEdit(reunion.get('id'));
+    },
+
+  },
 });
