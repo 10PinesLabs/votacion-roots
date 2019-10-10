235728f9859b801ae88c6493949e87d23d4c136f
ailen munoz <ailenmunozz@gmail.com>
2019-10-10 11:02:33 -0300

    Inicializa la reunion seleccionada en 0 y obtiene las duraciones

--- frontend/app/controllers/reuniones/list.js
+++ frontend/app/controllers/reuniones/list.js
@@ -6,6 +6,9 @@ import UserServiceInjected from "../../mixins/user-service-injected";
 import MinutaServiceInjected from "../../mixins/minuta-service-injected";
 
 export default Ember.Controller.extend(ReunionServiceInjected, MinutaServiceInjected, UserServiceInjected, NavigatorInjected, DuracionesServiceInjected, {
+  init(){
+    this._traerDuraciones().then( () => this.set('indiceSeleccionado', 0));
+  },
 
   reunionSeleccionada: Ember.computed('model.[]', 'indiceSeleccionado', function () {
     var indiceSeleccionado = this.get('indiceSeleccionado');
