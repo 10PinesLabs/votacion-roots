028ca7399d6defdd30a8654cd1fa8b5b5990cf1e
Angel Yan <angelyan96@gmail.com>
2021-03-18 16:32:36 -0300

    Remove logs used to debug database url

--- backend/src/main/java/ar/com/kfgodel/temas/config/HerokuConfig.java
+++ backend/src/main/java/ar/com/kfgodel/temas/config/HerokuConfig.java
@@ -4,11 +4,8 @@ import ar.com.kfgodel.dependencies.api.DependencyInjector;
 import ar.com.kfgodel.dependencies.impl.DependencyInjectorImpl;
 import ar.com.kfgodel.orm.api.config.DbCoordinates;
 import ar.com.kfgodel.orm.impl.config.ImmutableDbCoordinates;
-import ar.com.kfgodel.temas.application.TemasApplication;
 import ar.com.kfgodel.webbyconvention.api.auth.WebCredential;
 import com.heroku.sdk.jdbc.DatabaseUrl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.net.URISyntaxException;
 import java.util.Optional;
@@ -19,7 +16,6 @@ import java.util.function.Function;
  * Created by kfgodel on 13/03/16.
  */
 public abstract class HerokuConfig implements TemasConfiguration {
-  public static Logger LOG = LoggerFactory.getLogger(HerokuConfig.class);
 
   private DependencyInjector injector= DependencyInjectorImpl.create();
 
@@ -31,15 +27,10 @@ public abstract class HerokuConfig implements TemasConfiguration {
     } catch (URISyntaxException e) {
       throw new RuntimeException("Error accessing heroku jdbc url", e);
     }
-    String originalUrl = herokuCoordinates.jdbcUrl();
-    LOG.info("Database original url: {}", originalUrl);
-    String url = originalUrl + "?sslmode=require";
-    LOG.info("Database fixed url: {}", url);
+    String url = herokuCoordinates.jdbcUrl() + "?sslmode=require";
     String userName = herokuCoordinates.username();
     String password = herokuCoordinates.password();
-    DbCoordinates dbCoordinates = ImmutableDbCoordinates.createDeductingDialect(url, userName, password);
-    LOG.info("Database coordinates url: {}", dbCoordinates.getDbUrl());
-    return dbCoordinates;
+    return ImmutableDbCoordinates.createDeductingDialect(url, userName, password);
   }
 
   @Override
